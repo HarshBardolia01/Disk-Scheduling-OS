85eb5df64af62b66ac3947651cd3c5bd4bfad30b
Pranav Sirodaria <77057438+pranav-0@users.noreply.github.com>
2022-04-26 09:18:59 +0530

    Update CLOOK.js

--- JS/CLOOK.js
+++ JS/CLOOK.js
@@ -106,7 +106,7 @@ function clook(){
     {
         seek=seek+Math.abs(seek_sequence[i]-seek_sequence[i-1]);
     }
-    document.querySelector('.order').innerHTML ="Order of execution = " +dummy;
+    document.querySelector('.order').innerHTML ="Order of execution = " +seek_sequence;
 
     document.querySelector('.time').innerHTML ="Total Seek Time = "+seek;
 
